b065d2d7caa871d404994116812f168220609138
StemMajzel <andrej.zadnikar@gmail.com>
2015-12-16 13:32:39 +0100

    Modified output of client_test.php

--- client_test.php
+++ client_test.php
@@ -4,11 +4,14 @@ require 'vendor/autoload.php';
 
 $client = new JsonRPC\Client('http://127.0.0.1');
 
-$result = $client->execute('MSISDNLookup', [43454345]);
-echo '<pre>', var_export($result, true) , '</pre>';
+$num = 43454345;
+$result = $client->execute('MSISDNLookup', [$num]);
+echo '<pre>', 'number: ', $num, '<br />', var_export($result, true) , '</pre>';
 
-$result = $client->execute('MSISDNLookup', [3864060507056]);
-echo '<pre>', var_export($result, true) , '</pre>';
+$num = 3864060507056;
+$result = $client->execute('MSISDNLookup', [$num]);
+echo '<pre>', 'number: ', $num, '<br />', var_export($result, true) , '</pre>';
 
-$result = $client->execute('MSISDNLookup', ['5466h466']);
-echo '<pre>', var_export($result, true) , '</pre>';
+$num = '5466h466';
+$result = $client->execute('MSISDNLookup', [$num]);
+echo '<pre>', 'number: ', $num, '<br />', var_export($result, true) , '</pre>';
